6ca0caee0a59cdc3d6cbe2bc44f7e51c55dbb228
Evgeniy Khorev <e.khorev@MacBook-Pro.local>
2023-08-06 11:12:44 +0300

    Задача суммирование зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let salary = 0;
+  for (key in salaries)  {
+    if (typeof (salaries[key]) == "number" && Number.isNaN(salaries[key]) == false && Number.isFinite(salaries[key]) == true) {
+      salary = salary + salaries[key]
+    }
+  }
+  return salary
+  }
+
